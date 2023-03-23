761d7b9f63ab40e2248fdb64e35d43d814273c1f
Elber Canoles <elbercanoles@gmail.com>
2023-03-23 07:30:56 -0500

    feat: add vue js 3 to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+window.app = createApp({
+    setup() {
+        return {
+            message: 'Welcome to Your Vue.js App',
+        };
+    },
+    components: {
+        
+    },
+}).mount('#app');
