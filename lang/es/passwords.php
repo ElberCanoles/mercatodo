4cfc943cd8139f9ad88ba99327253af565cbf8fb
Elber Canoles <elbercanoles@gmail.com>
2023-03-23 16:21:56 -0500

    feat: publish lang folder

---
+++ lang/es/passwords.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Hemos enviado a su email un enlace de restablecimiento de contraseña.',
+    'throttled' => 'Espere antes de volver a intentarlo.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => "No se encontro un usuario registrado con este email.",
+    'current_password' => 'La contraseña proporcionada no coincide con su contraseña actual.',
+
+];
---
+++ lang/es/server.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'unavailable_service' => 'Servicio no disponible.',
+    'internal_error' => 'Error interno del servidor.',
+
+];
