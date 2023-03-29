7ee180f0f64a9893973788358a6eebb438436f12
Elber Canoles <elbercanoles@gmail.com>
2023-03-29 10:26:07 -0500

    feat: add user manage profile module
    Modules for updating personal data and password of the buyer user profile are implemented.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,17 @@
 import './bootstrap';
 import { createApp } from 'vue/dist/vue.esm-bundler';
+
 import LoginForm from '@/components/auth/LoginForm.vue';
 import RegisterForm from '@/components/auth/RegisterForm.vue'
 import VerifyEmailForm from '@/components/auth/VerifyEmailForm.vue'
 import ForgotPasswordForm from '@/components/auth/ForgotPasswordForm.vue'
 import ResetPasswordForm from '@/components/auth/ResetPasswordForm.vue'
+
 import AdminUsersTable from '@/components/admin/users/Table.vue'
 import AdminUsersEditForm from '@/components/admin/users/crud/EditForm.vue'
 import AdminProfileEditForm from '@/components/admin/profile/EditForm.vue'
+
+import BuyerProfileEditForm from '@/components/buyer/profile/EditForm.vue'
 
 
 window.app = createApp({
@@ -25,5 +29,6 @@ window.app = createApp({
         AdminUsersTable,
         AdminUsersEditForm,
         AdminProfileEditForm,
+        BuyerProfileEditForm,
     },
 }).mount('#app');
