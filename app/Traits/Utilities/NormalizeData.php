a612c8f2f0d891fc849855cd7495f261661815d9
Elber Canoles <elbercanoles@gmail.com>
2023-03-29 23:58:42 -0500

    docs: Trait NormalizeData

--- app/Traits/Utilities/NormalizeData.php
+++ app/Traits/Utilities/NormalizeData.php
@@ -9,21 +9,45 @@ use Illuminate\Support\Facades\Hash;
 trait NormalizeData
 {
 
+    /**
+     * Get formatted string with strtolower
+     *
+     * @param string $input
+     * @return string
+     */
     protected function normalizeStringUsingStrtolower(string $input): string
     {
         return strtolower(trim($input));
     }
 
+    /**
+     * Get formatted string with ucwords
+     *
+     * @param string $input
+     * @return string
+     */
     protected function normalizeStringUsingUcwords(string $input): string
     {
         return ucwords($this->normalizeStringUsingStrtolower($input));
     }
 
+    /**
+     * Get formatted string with ucfirst
+     *
+     * @param string $input
+     * @return string
+     */
     protected function normalizeStringUsingUcfirst(string $input): string
     {
         return ucfirst($this->normalizeStringUsingStrtolower($input));
     }
 
+    /**
+     * Get cipher hash using bcrypt algorithm
+     *
+     * @param string $input
+     * @return string
+     */
     protected  function normalizeStringUsingHash(string $input): string
     {
         return Hash::make($input);
