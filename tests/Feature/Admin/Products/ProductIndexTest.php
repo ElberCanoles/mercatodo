045cfcff90697a646c7aa7832955fe1c7f70f927
Elber Canoles <elbercanoles@gmail.com>
2023-04-18 21:19:02 -0500

    feat: add admin product index test

---
+++ tests/Feature/Admin/Products/ProductIndexTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Admin\Products;
+
+use App\Enums\User\RoleType;
+use App\Models\User;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductIndexTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    private User $admin;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(RoleSeeder::class);
+        $this->admin = User::factory()->create()->assignRole(RoleType::ADMINISTRATOR);
+    }
+
+
+    public function test_admin_can_access_to_products_list_screen(): void
+    {
+        $response = $this
+            ->actingAs($this->admin)
+            ->get(route('admin.products.index'));
+
+        $response->assertOk()
+            ->assertViewIs('admin.products.index');
+    }
+
+    public function test_admin_can_get_products_list_paginated_data(): void
+    {
+        $response = $this
+            ->actingAs($this->admin)
+            ->getJson(route('admin.products.index'));
+
+        $response->assertOk()
+            ->assertJsonStructure([
+                'current_page',
+                'data' => [],
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'links' => [],
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total'
+            ]);
+    }
+}
