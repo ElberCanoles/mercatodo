cfc8d4088dcd515a0d81fea9416ceb619fa33a1c
Elber Canoles <elbercanoles@gmail.com>
2023-04-26 22:01:37 -0500

    feat: add search on gallery products

--- app/Repositories/Product/ProductEloquentRepository.php
+++ app/Repositories/Product/ProductEloquentRepository.php
@@ -28,42 +28,50 @@ final class ProductEloquentRepository extends Repository implements ProductRepos
         $lengthPerPage = SystemParams::LENGTH_PER_PAGE;
 
         $query = $this->model::query()
-            ->select('id', 'name', 'slug', 'price', 'stock', 'status', 'created_at');
+            ->select(columns:  ['id', 'name', 'slug', 'price', 'stock', 'status', 'created_at']);
 
-        if ($this->isDefined($queryParams['name'] ?? null)) {
-            $query = $query->where('name', 'like', '%' . $queryParams['name'] . '%');
+        if ($this->isDefined(attribute: $queryParams['name'] ?? null)) {
+            $query = $query->where(column: 'name', operator: 'like', value: '%' . $queryParams['name'] . '%');
         }
 
-        if ($this->isDefined($queryParams['price'] ?? null)) {
-            $query = $query->where('price', 'like', '%' . $queryParams['price'] . '%');
+        if ($this->isDefined(attribute: $queryParams['price'] ?? null)) {
+            $query = $query->where(column: 'price', operator: 'like', value: '%' . $queryParams['price'] . '%');
         }
 
-        if ($this->isDefined($queryParams['stock'] ?? null)) {
-            $query = $query->where('stock', 'like', '%' . $queryParams['stock'] . '%');
+        if ($this->isDefined(attribute: $queryParams['minimum_price'] ?? null)) {
+            $query = $query->where(column: 'price', operator: '>=', value: $queryParams['minimum_price']);
         }
 
-        if ($this->isDefined($queryParams['status'] ?? null)) {
-            $query = $query->where('status', $queryParams['status']);
+        if ($this->isDefined(attribute: $queryParams['maximum_price'] ?? null)) {
+            $query = $query->where(column: 'price', operator: '<=', value: $queryParams['maximum_price']);
         }
 
-        if ($this->isDefined($queryParams['per_page'] ?? null) && $queryParams['per_page'] <= $lengthPerPage) {
+        if ($this->isDefined(attribute: $queryParams['stock'] ?? null)) {
+            $query = $query->where(column: 'stock', operator: 'like', value: '%' . $queryParams['stock'] . '%');
+        }
+
+        if ($this->isDefined(attribute: $queryParams['status'] ?? null)) {
+            $query = $query->where(column: 'status', value: $queryParams['status']);
+        }
+
+        if ($this->isDefined(attribute: $queryParams['per_page'] ?? null) && $queryParams['per_page'] <= $lengthPerPage) {
             $lengthPerPage = $queryParams['per_page'];
         }
 
-        return $query->orderBy('created_at', 'DESC')
-            ->orderBy('id', 'DESC')
+        return $query->orderBy(column: 'created_at', direction: 'DESC')
+            ->orderBy(column: 'id', direction: 'DESC')
             ->paginate($lengthPerPage)->through(fn ($product) => [
                 'name' => $product->name,
                 'description' => $product->description,
                 'images' => $product->images,
-                'price' => number_format($product->price),
-                'stock' => number_format($product->stock),
+                'price' => number_format(num: $product->price, decimal_separator: ',', thousands_separator: '.'),
+                'stock' => number_format(num: $product->stock, decimal_separator: ',', thousands_separator: '.'),
                 'status_key' => $product->status,
                 'status_value' => trans($product->status),
                 'created_at' => $product->created_at->format('d-m-Y'),
-                'show_url' => route('buyer.products.show', ['slug' => $product->slug]),
-                'edit_url' => route('admin.products.edit', ['product' => $product->id]),
-                'delete_url' => route('admin.products.destroy', ['product' => $product->id]),
+                'show_url' => route(name: 'buyer.products.show', parameters: ['slug' => $product->slug]),
+                'edit_url' => route(name: 'admin.products.edit', parameters: ['product' => $product->id]),
+                'delete_url' => route(name: 'admin.products.destroy', parameters: ['product' => $product->id]),
             ]);
     }
 
